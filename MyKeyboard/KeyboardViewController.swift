269bad74b5bdfad504c1afa8a1645018ba630ccc
pretzei <13136695713@163.com>
2016-05-10 18:30:07 +0800

    update

--- MyKeyboard/KeyboardViewController.swift
+++ MyKeyboard/KeyboardViewController.swift
@@ -26,12 +26,12 @@ class KeyboardViewController: UIInputViewController, keyboardDelegate {
         let keyboard: KB? = NSBundle.mainBundle().loadNibNamed("KB", owner: nil, options: nil).last as? KB;
         keyboard?.delegate = self
         keyboard?.frame = view.frame
-        keyboard?.sizeToFit()
+        //keyboard?.sizeToFit()
         self.view.addSubview(keyboard!);
-        var leftConstraint = NSLayoutConstraint(item: keyboard!, attribute: .Left, relatedBy: .Equal, toItem: self.view, attribute: .Left, multiplier: 1.0, constant: 0.0)
-        var bottomConstrait = NSLayoutConstraint(item: keyboard!, attribute: .Bottom, relatedBy: .Equal, toItem: self.view, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
-        var topConstraint = NSLayoutConstraint(item: keyboard!, attribute: .Top, relatedBy: .Equal, toItem: self.view, attribute: .Top, multiplier: 1.0, constant: 0.0)
-        var rightConstraint = NSLayoutConstraint(item: keyboard!, attribute: .Right, relatedBy: .Equal, toItem: self.view, attribute: .Right, multiplier: 1.0, constant: 0.0)
+        let leftConstraint = NSLayoutConstraint(item: keyboard!, attribute: .Left, relatedBy: .Equal, toItem: self.view, attribute: .Left, multiplier: 1.0, constant: 0.0)
+        let bottomConstrait = NSLayoutConstraint(item: keyboard!, attribute: .Bottom, relatedBy: .Equal, toItem: self.view, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
+        let topConstraint = NSLayoutConstraint(item: keyboard!, attribute: .Top, relatedBy: .Equal, toItem: self.view, attribute: .Top, multiplier: 1.0, constant: 0.0)
+        let rightConstraint = NSLayoutConstraint(item: keyboard!, attribute: .Right, relatedBy: .Equal, toItem: self.view, attribute: .Right, multiplier: 1.0, constant: 0.0)
         self.view.addConstraints([leftConstraint, bottomConstrait, topConstraint, rightConstraint])
         
     }
@@ -41,15 +41,15 @@ class KeyboardViewController: UIInputViewController, keyboardDelegate {
         // Dispose of any resources that can be recreated
     }
 
-    override func textWillChange(textInput: UITextInput) {
+    override func textWillChange(textInput: UITextInput?) {
         // The app is about to change the document's contents. Perform any preparation here.
     }
 
-    override func textDidChange(textInput: UITextInput) {
+    override func textDidChange(textInput: UITextInput?) {
         // The app has just changed the document's contents, the document context has been updated.
     
         var textColor: UIColor
-        var proxy = self.textDocumentProxy as! UITextDocumentProxy
+        let proxy = self.textDocumentProxy 
         if proxy.keyboardAppearance == UIKeyboardAppearance.Dark {
             textColor = UIColor.whiteColor()
         } else {
@@ -59,15 +59,15 @@ class KeyboardViewController: UIInputViewController, keyboardDelegate {
     }
     
     func insert(item: NSString) {
-        (self.textDocumentProxy as! UITextDocumentProxy).insertText(item as String)
+        (self.textDocumentProxy ).insertText(item as String)
     }
 
     func delete() {
-        (self.textDocumentProxy as! UITextDocumentProxy).deleteBackward()
+        (self.textDocumentProxy ).deleteBackward()
     }
     
     func enter() {
-        (self.textDocumentProxy as! UITextDocumentProxy).insertText("\n")
+        (self.textDocumentProxy ).insertText("\n")
     }
     
     func next() {
